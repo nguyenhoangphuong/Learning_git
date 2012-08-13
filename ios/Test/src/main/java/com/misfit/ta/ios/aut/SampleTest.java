02c39ebb06d09105e67cbdd2000bac95afa41665
Thinh Le <thinh@misfitwearables.com>
2012-08-13 17:34:55 +0700

    Sample test o

--- ios/Test/src/main/java/com/misfit/ta/ios/aut/SampleTest.java
+++ ios/Test/src/main/java/com/misfit/ta/ios/aut/SampleTest.java
@@ -14,14 +14,16 @@ import com.misfit.ta.utils.ShortcutsTyper;
  * 
  * @author qa
  */
-public class C1157 extends AutomationTest {
+public class SampleTest extends AutomationTest {
 
-  @Test(groups = {"ios", "Perry"})
-  public void c1157() throws InterruptedException, StopConditionException {
-//	  AppHelper.install(AppHelper.getCurrentUdid(), AppHelper.getAppPath());
-//	  ShortcutsTyper.delayTime(3000);
+  @Test(groups = {"ios", "Perry", "Perry_1"})
+  public void sampleTest() throws InterruptedException, StopConditionException {
+	  AppHelper.install(AppHelper.getCurrentUdid(), AppHelper.getAppPath());
+	  ShortcutsTyper.delayTime(3000);
 	  AppHelper.launchInstrument(AppHelper.getCurrentUdid(), AppHelper.getAppPath(), 
-				 "script/C1157.js");
+				 "script/test.js");
+//	  Logger logger = Util.setupLogger(SampleTest.class);
+//	  logger.info("This is a test message");
   }
   
   
