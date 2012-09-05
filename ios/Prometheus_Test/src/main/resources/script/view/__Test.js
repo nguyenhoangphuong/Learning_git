c87140b75e2e0b19c0e1c57bbdd5806d648c8e01
Hai Nguyen <hainguyen@misfitwearables.com>
2012-09-05 10:43:01 +0700

    Sample test

--- ios/Prometheus_Test/src/main/resources/script/view/__Test.js
+++ ios/Prometheus_Test/src/main/resources/script/view/__Test.js
@@ -1,39 +1,44 @@
-#import "Agreement.js"
-#import "SignUp.js"
-#import "Legal.js"
+#import "_Navigator.js"
 #import "../core/testcaseBase.js"
 
-function test1()
-{
-	UIALogger.logStart("Test Demo");
-	var agreement = new Agreement();
-	agreement.tapAgree();
 
-	var signup = new SignUp();
+function testSignUp()
+{
+	signup = new SignUp();
+	signup.pressLicenceAgreement();
+	if(!signup.closeLicenceAgreement())
+		fail();
+	signup.closeLicenceAgreement();
+	
 	signup.fillEmailAndSubmit("");
-
-	var error = signup.getErrorMessage();
+	error = signup.getErrorMessage();
+	
 	log(error);
-
-	if(error == signup.MsgEmpty)
-		UIALogger.logPass("Passed");
-	else
-		UIALogger.logFail("Failed");
 }
 
-function test2()
+function testUserInfo()
 {
-	start();
-	
-	legal = new Legal();	
-	legal.setSex("male");
-	legal.setSex("female");
-	legal.setUnit("si");
-	legal.setUnit("us");
-	legal.setInfo(18, 43, 0.5, 2, 0.85);
-	legal.setInfo(18, 145, 0.3, "7'", "3\"");
-	
-	pass();
+	ui = new UserInfo();	
+	//ui.setSex("male");
+	//ui.setSex("female");
+	ui.setUnit("si");
+	ui.setUnit("us");
+	//ui.setInfo(18, 46, 0.5, 1, 0.55);
+	//ui.setInfo(18, 95, 0.3, "6'", "8\"");
+	//ui.changeWeight(-100);
+	//ui.changeHeight(100);
 }
 
-test2()
+function testGoalProgress()
+{
+	goal = new GoalProgress();
+	goal.scrollToDayProgress();
+	goal.scrollToWeekProgress();
+	goal.scrollToHistory();
+}
+
+start();
+
+testUserInfo();
+
+pass();
