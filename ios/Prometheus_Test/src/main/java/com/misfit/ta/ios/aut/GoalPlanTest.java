76dfbddc47ceaf3c278c8abbee55b166736b0cdd
Thinh Le <thinh@misfitwearables.com>
2012-09-14 18:02:36 +0700

    Add testcases for goal plan

---
+++ ios/Prometheus_Test/src/main/java/com/misfit/ta/ios/aut/GoalPlanTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates and open the template in the editor.
+ */
+package com.misfit.ta.ios.aut;
+
+import org.graphwalker.exceptions.StopConditionException;
+import org.testng.annotations.Test;
+
+import com.misfit.ios.AppHelper;
+import com.misfit.ta.ios.AutomationTest;
+import com.misfit.ta.utils.ShortcutsTyper;
+
+/**
+ * 
+ * @author qa
+ */
+public class GoalPlanTest extends AutomationTest {
+
+  @Test(groups = {"ios", "Prometheus", "goalplan"})
+  public void sampleTest() throws InterruptedException, StopConditionException {
+	  AppHelper.cleanCache();
+	  ShortcutsTyper.delayTime(1000);
+	  AppHelper.launchInstrument(AppHelper.getCurrentUdid(), AppHelper.getAppPath(), 
+				 "script/testcases/goalplan/goalplan_tcs.js");
+  }
+ 
+  
+  
+}
