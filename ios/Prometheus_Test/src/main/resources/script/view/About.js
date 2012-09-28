8b8a47d0a110decaef7ef71f5159ad887bd32365
Tung Nguyen <tung@misfitwearables.com>
2012-09-28 18:27:16 +0700

    Revise code of About.js for not containing lines longer than 80 chars Change and add methods to comform the new flow of About (aka Settings)

--- ios/Prometheus_Test/src/main/resources/script/view/About.js
+++ ios/Prometheus_Test/src/main/resources/script/view/About.js
@@ -2,16 +2,26 @@
 #import "_AlertHandler.js"
 
 /*
-About functions:
-- isVisible()	:	check if the current view is About
-- rateApp()		:	tap Rate button
-- getSupport()	:	tap Email button and handler message box
-- likeApp()		:	tap Like button
-- resetPlan(confirm)	:	tap Reset button and choose Yes/No when alert shown up
+About (aka Settings) functions:
+- isVisible()		: check if current view is About
+- isSupportView()	: check if current view is Support
+- isTroublemaker()	: check if current user is a troublemaker
+- hasSignedIn()		: check if current user has signed in
+- goToProfile		: tap "User Profile" button
+- rateApp()			: tap Rate button
+- tapSupport()		: tap Support button
+	- emailSupport(): tap "Email support" button
+	- likePage()	: tap "Like our page" button
+	- goToWebsite()	: tap Website button
+	- backToAbout()	: tap Back button in Support screen
+- tapFeedback()		: tap "JIRA Feedback" button (w/ troublemaker)
+- resetPlan(confirm)	: tap Reset button and choose Yes/No when alert shown up
 	+ resetPlan("yes")
 	+ resetPlan("no")
-- isNoEmailAlertShown()			:	check if the NoEmail alert is shown up
-- isResetConfirmAlertShown()	:	check if the ResetConfirm alert is shown up
+- signOut()			: tap "Sign out" button (w/ user having logged in)
+- signUp()			: tap "Sign up" button (w/ user having not logged in)
+- isNoEmailAlertShown()			: check if the NoEmail alert is shown up
+- isResetConfirmAlertShown()	: check if the ResetConfirm alert is shown up
 */
 
 function About()
@@ -19,49 +29,122 @@ function About()
 	// Private fields
 	var window = app.mainWindow();
 	var mainView = window.scrollViews()[0];
+	var supportView = window.scrollViews()[0]; // todo: change this
 	
-	var rate = mainView.buttons()[0];
-	var email = mainView.buttons()[1];
-	var like = mainView.buttons()[2];
-	var reset = mainView.buttons()[3];
+	// todo: check indexes
+	var profile = mainView.buttons()[0];
+	var rate = mainView.buttons()[1];
+	var support = mainView.buttons()[2];
+	var email = supportView.buttons()[0];
+	var like = supportView.buttons()[1];
+	var website = supportView.buttons()[2];
+	var back = supportView.buttons()[3];
+	var feedback = mainView.buttons()[3];
+	var reset = mainView.buttons()[4];
+	var signOut = mainView.buttons()[5];
+	var signUp = mainView.buttons()[6];
 	
 	// Methods
 	this.isVisible = isVisible;
+	this.isSupportView = isSupportView;
+	this.isTroublemaker = isTroublemaker;
+	this.hasSignedIn = hasSignedIn;
 	
+	this.goToProfile = goToProfile; 
 	this.rateApp = rateApp;
-	this.getSupport = getSupport;
-	this.likeApp = likeApp;
+	this.tapSupport = tapSupport;
+	this.emailSupport = emailSupport;
+	this.likePage = likePage;
+	this.goToWebsite = goToWebsite;
+	this.backToAbout = backToAbout;
+	this.tapFeedback = tapFeedback;
 	this.resetPlan = resetPlan;
+	this.signOut = signOut;
+	this.signUp = signUp;
 	
 	this.isNoEmailAlertShown = isNoEmailAlertShown;
 	this.isResetConfirmAlertShown = isResetConfirmAlertShown;
 	
-	// Methods definition
+	// Method definitions
 	function isVisible()
 	{
+		// todo: refine
 		page = window.pageIndicators()[0].value();
 		return page == "page 1 of 3" && rate.isValid() && rate.isVisible();
 	}
-
+	
+	function isSupportView()
+	{
+		// todo: refine
+		return email.isValid() && email.isVisible() &&
+			like.isValid() && like.isVisible();
+	}
+	
+	function isTroublemaker()
+	{
+		// todo: refine
+		return feedback.isValid() && feedback.isVisible();
+	}
+	
+	function hasSignedIn()
+	{
+		// todo: refine
+		return signOut.isValid() && signOut.isVisible();
+	}
+	
+	function goToProfile()
+	{
+		// todo: refine
+		profile.tap();
+	}
 	
 	function rateApp()
 	{
+		// todo: refine
 		rate.tap();
 	}
 	
-	function getSupport()
+	function tapSupport()
 	{
-		email.tap();
-		wait(3);
+		// todo: refine
+		support.tap();
 	}
 	
-	function likeApp()
+	function emailSupport()
 	{
+		// todo: change this function, use supportView
+		email.tap();
+		wait(3); // what is this for?
+	}
+	
+	function likePage()
+	{
+		// todo: change this function, use supportView
 		like.tap();
+	}
+	
+	function goToWebsite()
+	{
+		// todo: change this function, use supportView
+		website.tap();
+	}
+	
+	function backToAbout()
+	{
+		// todo: change this function, use supportView
+		back.tap();
+	}
+	
+	function tapFeedback()
+	{
+		// todo: refine, maybe add methods to feedback
+		if (isTroublemaker())
+			feedback.tap();
 	}
 	
 	function resetPlan(yes)
 	{
+		// todo: check
 		reset.tap();
 		wait(1);
 		
@@ -77,16 +160,32 @@ function About()
 		}
 	}
 	
+	function signOut()
+	{
+		// todo: refine
+		if (hasSignedIn())
+			signOut.tap();
+	}
+	
+	function signUp()
+	{
+		// todo: refine, maybe add more steps, just maybe
+		if (!hasSignedIn())
+			signUp.tap();
+	}
 	
 	function isNoEmailAlertShown()
 	{
+		// todo: check
 		log("checking: " + alert.NoEmail);
 		return alert.alertTitle != null && alert.alertTitle == alert.NoEmail;
 	}
 	
 	function isResetConfirmAlertShown()
 	{
+		// todo: check
 		log("checking: " + alert.ResetConfirm);
-		return alert.alertTitle != null && alert.alertTitle == alert.ResetConfirm;
+		return alert.alertTitle != null &&
+			alert.alertTitle == alert.ResetConfirm;
 	}
 }
