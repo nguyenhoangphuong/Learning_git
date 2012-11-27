3479c79e3e0551f92f989448c89ec48bd5718f56
tvkhoa <khoathai@misfitwearables.com>
2012-11-27 18:47:41 +0700

    add server script

---
+++ server/backend/config.sh
@@ -0,0 +1,12 @@
+PARAMS_N=({'10','20','30','40','50','60','70','80','90','100'})
+PARAMS_C=('5')
+
+LINK_SIGNUP="http://192.168.1.102:3000/shine/v2/signup"
+LINK_LOGIN="http://192.168.1.102:3000/shine/v2/login"
+LINK_SYNC="http://192.168.1.102:3000/shine/v2/sync"
+
+FOLDER_ROOT="/Users/khoathai/Documents/testab"
+
+FILE_TEXT_SIGNUP="$FOLDER_ROOT/textsignup.txt"
+FILE_TEXT_LOGIN="$FOLDER_ROOT/textlogin.txt"
+FILE_TEXT_SYNC="$FOLDER_ROOT/textsync.txt"
---
+++ server/backend/stressTest.sh
@@ -0,0 +1,105 @@
+source "config.sh"
+
+
+
+TIMESTAMP=`date +%s`
+FOLDER_RESULT="$FOLDER_ROOT/Result-$TIMESTAMP"
+FILE_TEMP="$FOLDER_RESULT/temp.txt"
+FILE_SUMMARY="$FOLDER_RESULT/summary.csv"
+
+
+
+# Create result folder
+mkdir -p "$FOLDER_RESULT"
+
+
+
+function runsignup
+{
+    # Variables
+    TOTAL_C=${#PARAMS_C[@]}
+    TOTAL_N=${#PARAMS_N[@]}
+    echo $TOTAL_C
+    echo $TOTAL_N
+
+    echo "Concurency,Total Requests,Total time (sec),failed requests,requests per sec,time per request (ms),time per request (ms) (across all concurrent requests)" > "$FILE_SUMMARY"
+    # Run with each concurrent
+    for (( i=0;i<$TOTAL_C;i++)); do
+        echo "Concurent=${PARAMS_C[$i]}"
+        #Run with each n
+        for (( j=0;j<$TOTAL_N;j++)); do
+            echo "Requests = ${PARAMS_N[$j]}"
+            # Run command --> file temp
+            #Check variable
+            PARAM_C=${PARAMS_C[${i}]}
+            PARAM_N=${PARAMS_N[${j}]}
+
+            #Execute script
+            eval ab -n "$PARAM_N" -c "$PARAM_C" -p "$FILE_TEXT_SIGNUP" -T 'application/x-www-form-urlencoded' -d "$LINK_SIGNUP" > "$FILE_TEMP" 
+
+            #sleep 10s
+            echo "sleep 10s"
+            sleep 10 
+
+            # Grepfile : concurency,requests,time (sec),failed requests,requests per sec,time per request (ms),time per request (ms) (across all concurrent requests)
+            # Grep -> Variable
+            TIME_TAKEN=`eval cat "$FILE_TEMP" | grep "Time taken for tests:" | cut -d ' ' -f 7 `
+            FAIL_REQUESTS=`eval cat "$FILE_TEMP" | grep "Failed requests:" | cut -d ' ' -f 10 `
+            REQS_PER_SEC=`eval cat "$FILE_TEMP" | grep "Requests per second:" | cut -d ' ' -f 7 `
+            TIME_PER_REQ=`eval cat "$FILE_TEMP" | grep "Time per request:" | grep "(mean)" | cut -d ' ' -f 10 `
+            TIME_PER_REQ_CONCURRENT=`eval cat "$FILE_TEMP" | grep "Time per request:" | grep "(mean, across all concurrent requests)" | cut -d ' ' -f 10`
+
+            #Write variables to file
+            echo "${PARAMS_C[$i]},${PARAMS_N[$j]},$TIME_TAKEN,$FAIL_REQUESTS,$REQS_PER_SEC,$TIME_PER_REQ,$TIME_PER_REQ_CONCURRENT" >> "$FILE_SUMMARY"
+
+            # clear file temp
+            rm -rf "$FILE_TEMP"
+        done
+    done 
+}
+
+function runlogin
+{
+    # Variables
+    TOTAL_C=${#PARAMS_C[@]}
+    TOTAL_N=${#PARAMS_N[@]}
+    echo $TOTAL_C
+    echo $TOTAL_N
+
+    echo "Concurency,Total Requests,Total time (sec),failed requests,requests per sec,time per request (ms),time per request (ms) (across all concurrent requests)" > "$FILE_SUMMARY"
+    # Run with each concurrent
+    for (( i=0;i<$TOTAL_C;i++)); do
+        echo "Concurent=${PARAMS_C[$i]}"
+        #Run with each n
+        for (( j=0;j<$TOTAL_N;j++)); do
+            echo "Requests = ${PARAMS_N[$j]}"
+            # Run command --> file temp
+            #Check variable
+            PARAM_C=${PARAMS_C[${i}]}
+            PARAM_N=${PARAMS_N[${j}]}
+
+            #Execute script
+            eval ab -n "$PARAM_N" -c "$PARAM_C" -p "$FILE_TEXT_LOGIN" -T 'application/x-www-form-urlencoded' -d "$LINK_LOGIN" > "$FILE_TEMP" 
+
+            #sleep 10s
+            echo "sleep 10s"
+            sleep 10 
+
+            # Grepfile : concurency,requests,time (sec),failed requests,requests per sec,time per request (ms),time per request (ms) (across all concurrent requests)
+            # Grep -> Variable
+            TIME_TAKEN=`eval cat "$FILE_TEMP" | grep "Time taken for tests:" | cut -d ' ' -f 7 `
+            FAIL_REQUESTS=`eval cat "$FILE_TEMP" | grep "Failed requests:" | cut -d ' ' -f 10 `
+            REQS_PER_SEC=`eval cat "$FILE_TEMP" | grep "Requests per second:" | cut -d ' ' -f 7 `
+            TIME_PER_REQ=`eval cat "$FILE_TEMP" | grep "Time per request:" | grep "(mean)" | cut -d ' ' -f 10 `
+            TIME_PER_REQ_CONCURRENT=`eval cat "$FILE_TEMP" | grep "Time per request:" | grep "(mean, across all concurrent requests)" | cut -d ' ' -f 10`
+
+            #Write variables to file
+            echo "${PARAMS_C[$i]},${PARAMS_N[$j]},$TIME_TAKEN,$FAIL_REQUESTS,$REQS_PER_SEC,$TIME_PER_REQ,$TIME_PER_REQ_CONCURRENT" >> "$FILE_SUMMARY"
+
+            # clear file temp
+            rm -rf "$FILE_TEMP"
+        done
+    done 
+}
+
+runlogin
