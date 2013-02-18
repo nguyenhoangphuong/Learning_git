30027269dcab7a1b8c5c685df959765fa69d19a9
Tung Nguyen <tung@misfitwearables.com>
2013-02-18 16:23:57 +0700

    Added "active" option (values: true/false) to select which source/log to be logged 	change code to manipulate "active" option:   server/aws/logAggregator/LogAggregator.py 	add "active" option:   server/aws/logAggregator/config.json 	add "active" option:   server/aws/logAggregator/config_jenkins.json

--- server/aws/logAggregator/LogAggregator.py
+++ server/aws/logAggregator/LogAggregator.py
@@ -22,15 +22,17 @@ dest = data["destination"] + "/" + date + "_logs"
 sources = data["sources"]
 
 for source in sources:
-	for log in source["logs"]:
-		cmd1 = "mkdir -p %s/%s" % (dest, log["type"])
-		print source["key"]
-		
-		cmd2 = "scp "
-		if (source["key"] != ""):
-			cmd2 = cmd2 + " -i %s " %(source["key"])
+	if (source["active"] == True):
+		for log in source["logs"]:
+			if (log["active"] == True):
+				cmd1 = "mkdir -p %s/%s" % (dest, log["type"])
+				print source["key"]
+				
+				cmd2 = "scp "
+				if (source["key"] != ""):
+					cmd2 = cmd2 + " -i %s " %(source["key"])
 
-		cmd2 = cmd2 + "-r %s@%s:%s %s" % (source["user"], source["host"], log["path"], dest + "/" + log["type"])
-		print cmd2
-		os.system(cmd1)
-		os.system(cmd2)
+				cmd2 = cmd2 + "-r %s@%s:%s %s" % (source["user"], source["host"], log["path"], dest + "/" + log["type"])
+				print cmd2
+				os.system(cmd1)
+				os.system(cmd2)
