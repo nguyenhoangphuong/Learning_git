5adc5c21cf53b9d20b3933df33cd59ae64070c8a
thy-misfit <thy@misfitwearables.com>
2013-03-04 16:46:25 +0700

    MBT Graph and test implementation for SignIn

--- android/Prometheus_Test_Android/src/main/java/com/misfit/ta/android/gui/SignIn.java
+++ android/Prometheus_Test_Android/src/main/java/com/misfit/ta/android/gui/SignIn.java
@@ -2,6 +2,7 @@ package com.misfit.ta.android.gui;
 
 import com.misfit.ta.android.Gui;
 import com.misfit.ta.android.ViewUtils;
+import com.misfit.ta.utils.ShortcutsTyper;
 
 public class SignIn {
 
@@ -14,25 +15,43 @@ public class SignIn {
 	}
 
 	public static boolean isInitViewVisible() {
+		ShortcutsTyper.delayTime(1000);
 		return ViewUtils.findView("Button", "mID", "id/buttonHaveAccount", 0) != null;
 	}
 
 	public static boolean isSignInVisible() {
+		ShortcutsTyper.delayTime(1000);
 		return ViewUtils.findView("EditText", "mID", "id/editPassword", 0) != null;
 	}
 
-	private void fillSignIn(String email, String password) {
+	public static void fillSignIn(String email, String password) {
 		Gui.touchAView("EditText", "mID", "id/editEmail");
 		Gui.type(email);
 		Gui.touchAView("EditText", "mID", "id/editPassword");
 		Gui.type(password);
+		Gui.pressBack();
 	}
 
-	public void signIn() {
-		Gui.touchAView("Button", "mID", "id/buttonHaveAccount");
-		fillSignIn("test27@thy.com", "test12");
-		Gui.pressBack();
+	public static void pressNext() {
 		Gui.touchAView("ImageButton", "mID", "id/buttonNext");
+	}
+
+	public static boolean hasInvalidEmailMessage() {
+		ShortcutsTyper.delayTime(500);
+		return ViewUtils.findView("TextView", "mID", "id/textErrorMessage", 0).text.equals("\\nInvalid email");
+	}
+
+	public static boolean hasInvalidPasswordMessage() {
+		ShortcutsTyper.delayTime(500);
+		return ViewUtils.findView("TextView", "mID", "id/textErrorMessage", 0).text.equals("\\nSorry, the password should have at least 6 characters, at least 1 digit and 1 letter");
+	}
+	public static boolean hasIncorrectEmailPasswordMessage() {
+		ShortcutsTyper.delayTime(500);
+		return ViewUtils.findView("TextView", "mID", "id/textErrorMessage", 0).text.equals("Error\\nIncorrect email or password");
+	}
+	
+	public static void tapForgotPassword() {
+		Gui.touchAView("Button", "mID", "id/buttonForgotPassword");
 	}
 
 }
--- android/Prometheus_Test_Android/src/main/java/com/misfit/ta/android/modelapi/SignInAPI.java
+++ android/Prometheus_Test_Android/src/main/java/com/misfit/ta/android/modelapi/SignInAPI.java
@@ -11,51 +11,164 @@ import com.misfit.ta.modelAPI.ModelAPI;
 import com.misfit.ta.utils.ShortcutsTyper;
 
 public class SignInAPI extends ModelAPI {
-    public SignInAPI(AutomationTest automation, File model, boolean efsm, PathGenerator generator, boolean weight) {
-        super(automation, model, efsm, generator, weight);
-    }
+	public SignInAPI(AutomationTest automation, File model, boolean efsm,
+			PathGenerator generator, boolean weight) {
+		super(automation, model, efsm, generator, weight);
+	}
 
-    /**
-     * This method implements the Edge 'e_ChooseSignUp'
-     * 
-     */
-    public void e_ChooseSignIn() {
-        SignIn.chooseSignIn();
+	/**
+	 * This method implements the Edge 'e_ChooseSignIn'
+	 * 
+	 */
+	public void e_ChooseSignIn() {
+		SignIn.chooseSignIn();
+	}
+	
+	/**
+	   * This method implements the Edge 'e_ClearFields1'
+	   * 
+	   */
+	  public void e_ClearFields1() {
+		  SignIn.pressBack();
+		  ShortcutsTyper.delayTime(1000);
+		  SignIn.chooseSignIn();
+	  }
 
-    }
 
-    /**
-     * This method implements the Edge 'e_Init'
-     * 
-     */
-    public void e_Init() {
-        
-    }
 
-    /**
-     * This method implements the Edge 'e_PressBack'
-     * 
-     */
-    public void e_PressBack() {
-        SignIn.pressBack();
+
+	  /**
+	   * This method implements the Edge 'e_ClearFields2'
+	   * 
+	   */
+	  public void e_ClearFields2() {
+		  SignIn.pressBack();
+		  ShortcutsTyper.delayTime(1000);
+		  SignIn.chooseSignIn();
+	  }
+
+
+
+
+	  /**
+	   * This method implements the Edge 'e_ClearFields3'
+	   * 
+	   */
+	  public void e_ClearFields3() {
+		  SignIn.pressBack();
+		  ShortcutsTyper.delayTime(1000);
+		  SignIn.chooseSignIn(); 
+	  }
+
+
+	/**
+	 * This method implements the Edge 'e_FillCorrectEmailPassword'
+	 * 
+	 */
+	public void e_FillCorrectEmailPassword() {
+		SignIn.fillSignIn("test27@thy.com", "test12");
+	}
+
+	/**
+	 * This method implements the Edge 'e_FillIncorrectEmailOrPassword'
+	 * 
+	 */
+	public void e_FillIncorrectEmailOrPassword() {
+		SignIn.fillSignIn("test27@thythy.com", "test12");
+		SignIn.pressNext();
+	}
+
+	/**
+	 * This method implements the Edge 'e_FillInvalidEmail'
+	 * 
+	 */
+	public void e_FillInvalidEmail() {
+		SignIn.fillSignIn("test27.com", "");
+		SignIn.pressNext();
+	}
+
+	/**
+	 * This method implements the Edge 'e_FillInvalidPassword'
+	 * 
+	 */
+	public void e_FillInvalidPassword() {
+		SignIn.fillSignIn("test27@thy.com", "teste");
+		SignIn.pressNext();
+	}
+
+	/**
+	 * This method implements the Edge 'e_Init'
+	 * 
+	 */
+	public void e_Init() {
+		// TODO:
+	}
+
+	/**
+	 * This method implements the Edge 'e_PressBack'
+	 * 
+	 */
+	public void e_PressBack() {
+		SignIn.pressBack();
         ShortcutsTyper.delayTime(500);
         SignIn.pressBack();
-    }
+	}
 
-    /**
-     * This method implements the Vertex 'v_InitView'
-     * 
-     */
-    public void v_InitialView() {
-        Assert.assertTrue(SignIn.isInitViewVisible(), "Init view is not visible");
-    }
+	/**
+	 * This method implements the Vertex 'v_HomeScreen'
+	 * 
+	 */
+	public void v_HomeScreen() {
+		// TODO:
+	}
 
-    /**
-     * This method implements the Vertex 'v_UserInfo'
-     * 
-     */
-    public void v_SignInVisible() {
-        Assert.assertTrue(SignIn.isSignInVisible(), "Sign in is not visible");
-    }
+	/**
+	 * This method implements the Vertex 'v_IncorrectSignIn'
+	 * 
+	 */
+	public void v_IncorrectSignIn() {
+		Assert.assertTrue(SignIn.hasIncorrectEmailPasswordMessage(), "Invalid Email Or Password");
+	}
+
+	/**
+	 * This method implements the Vertex 'v_InitialView'
+	 * 
+	 */
+	public void v_InitialView() {
+		Assert.assertTrue(SignIn.isInitViewVisible(),
+				"Init view is not visible");
+	}
+
+	/**
+	 * This method implements the Vertex 'v_InvalidEmail'
+	 * 
+	 */
+	public void v_InvalidEmail() {
+		Assert.assertTrue(SignIn.hasInvalidEmailMessage(), "Invalid Email");
+	}
+
+	/**
+	 * This method implements the Vertex 'v_InvalidPassword'
+	 * 
+	 */
+	public void v_InvalidPassword() {
+		Assert.assertTrue(SignIn.hasInvalidPasswordMessage(), "Invalid Password");
+	}
+
+	/**
+	 * This method implements the Vertex 'v_SignInVisible'
+	 * 
+	 */
+	public void v_SignInVisible() {
+		Assert.assertTrue(SignIn.isSignInVisible(), "Sign in is not visible");
+	}
+
+	/**
+	 * This method implements the Vertex 'v_SuccessfulSignIn'
+	 * 
+	 */
+	public void v_SuccessfulSignIn() {
+		SignIn.pressNext();
+	}
 
 }
