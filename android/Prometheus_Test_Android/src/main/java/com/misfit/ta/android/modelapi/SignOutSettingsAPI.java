259ea8e4557bb3d8994a965f8aef2e6b8941302e
Tung Nguyen <tung@misfitwearables.com>
2013-03-08 15:40:16 +0700

    added SignOutSettingsAPI.java template

---
+++ android/Prometheus_Test_Android/src/main/java/com/misfit/ta/android/modelapi/SignOutSettingsAPI.java
@@ -0,0 +1,170 @@
+package com.misfit.ta.android.modelapi;
+
+import java.io.File;
+
+import org.graphwalker.generators.PathGenerator;
+import org.testng.Assert;
+
+import com.misfit.ta.android.AutomationTest;
+import com.misfit.ta.android.Gui;
+import com.misfit.ta.android.ViewUtils;
+import com.misfit.ta.android.aut.DefaultStrings;
+import com.misfit.ta.android.gui.HomeScreen;
+import com.misfit.ta.android.gui.Settings;
+import com.misfit.ta.android.gui.SignIn;
+import com.misfit.ta.android.gui.SignUp;
+import com.misfit.ta.android.gui.Helper.Helper;
+import com.misfit.ta.android.hierarchyviewer.scene.ViewNode;
+import com.misfit.ta.modelAPI.ModelAPI;
+
+public class SignOutSettingsAPI extends ModelAPI {
+	public SignOutSettingsAPI(AutomationTest automation, File model, boolean efsm, PathGenerator generator, boolean weight) {
+    super(automation, model, efsm, generator, weight);
+  }
+
+
+
+  /**
+   * This method implements the Edge 'e_ChangeProfile'
+   * 
+   */
+  public void e_ChangeProfile() {
+   //TODO: 
+  }
+
+
+
+
+  /**
+   * This method implements the Edge 'e_GoToProfile'
+   * 
+   */
+  public void e_GoToProfile() {
+   //TODO: 
+  }
+
+
+
+
+  /**
+   * This method implements the Edge 'e_GoToSettings'
+   * 
+   */
+  public void e_GoToSettings() {
+   //TODO: 
+  }
+
+
+
+
+  /**
+   * This method implements the Edge 'e_Init'
+   * 
+   */
+  public void e_Init() {
+   //TODO: 
+  }
+
+
+
+
+  /**
+   * This method implements the Edge 'e_SignIn'
+   * 
+   */
+  public void e_SignIn() {
+   //TODO: 
+  }
+
+
+
+
+  /**
+   * This method implements the Edge 'e_TapCancel'
+   * 
+   */
+  public void e_TapCancel() {
+   //TODO: 
+  }
+
+
+
+
+  /**
+   * This method implements the Edge 'e_TapSignOut'
+   * 
+   */
+  public void e_TapSignOut() {
+   //TODO: 
+  }
+
+
+
+
+  /**
+   * This method implements the Edge 'e_TapUpload'
+   * 
+   */
+  public void e_TapUpload() {
+   //TODO: 
+  }
+
+
+
+
+  /**
+   * This method implements the Vertex 'v_ConfirmationPopup'
+   * 
+   */
+  public void v_ConfirmationPopup() {
+   //TODO: 
+  }
+
+
+
+
+  /**
+   * This method implements the Vertex 'v_HomeScreen'
+   * 
+   */
+  public void v_HomeScreen() {
+   //TODO: 
+  }
+
+
+
+
+  /**
+   * This method implements the Vertex 'v_InitialView'
+   * 
+   */
+  public void v_InitialView() {
+   //TODO: 
+  }
+
+
+
+
+  /**
+   * This method implements the Vertex 'v_ProfileScreen'
+   * 
+   */
+  public void v_ProfileScreen() {
+   //TODO: 
+  }
+
+
+
+
+  /**
+   * This method implements the Vertex 'v_SettingsScreen'
+   * 
+   */
+  public void v_SettingsScreen() {
+   //TODO: 
+  }
+
+
+
+}
+
