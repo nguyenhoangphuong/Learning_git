fd45c3e0cb1215b91d5b20ce0c5b7cdac5aa1c3e
Hai Nguyen <hainguyen@misfitwearables.com>
2013-04-30 10:17:31 +0700

    Try to run old version of test

---
+++ ios/Prometheus_Test/src/main/java/com/misfit/ta/ios/tests/OldVersionTest.java
@@ -0,0 +1,30 @@
+package com.misfit.ta.ios.tests;
+
+import org.graphwalker.exceptions.StopConditionException;
+import org.testng.annotations.Test;
+
+import com.misfit.ios.AppHelper;
+import com.misfit.ta.utils.ShortcutsTyper;
+
+public class OldVersionTest 
+{
+	// sign in client and server validation
+	@Test(groups = { "ios", "OldInstrument", "signin" })
+	public void SignInValidation() throws InterruptedException, StopConditionException
+	{
+		AppHelper.cleanCache();
+		ShortcutsTyper.delayTime(1000);	
+		AppHelper.launchInstrument(AppHelper.getCurrentUdid(),
+				AppHelper.getAppPath(), "script/automation/signup/signin-validation.js");
+	}
+	
+	// sign in - forgot password validation
+	@Test(groups = { "ios", "OldInstrument", "signin", "forgotpassword" })
+	public void ForgotPasswordValidation() throws InterruptedException, StopConditionException
+	{
+		AppHelper.cleanCache();
+		ShortcutsTyper.delayTime(1000);	
+		AppHelper.launchInstrument(AppHelper.getCurrentUdid(),
+				AppHelper.getAppPath(), "script/automation/signup/forgotpassword-validation.js");
+	}
+}
