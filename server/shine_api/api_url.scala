ca2d798eec7ef4a2251459055ab326480d4cd739
tung <tung@misfitwearables.com>
2013-06-19 16:32:53 -0700

    remove Python script (for MongoDB test) and Scala scrips (for Shine API test) to move to server_profiling branch

--- server/shine_api/api_url.scala
+++
@@ -1,8 +0,0 @@
-package Shine
-
-object APIURL {
-	val apiUrl = Map("baseUrl" -> "https://misfit-shine-api-test-1663549078.us-east-1.elb.amazonaws.com",
-		"signup" -> "/shine/v7/signup",
-		"login" -> "/shine/v7/login",
-		"logout" -> "/shine/v7/logout")
-}
