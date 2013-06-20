193fdb0faa0cbe7d7a782956000b869eceafca93
tung <tung@misfitwearables.com>
2013-06-19 20:29:28 -0700

    changed signup simulation: put intructions into methods

--- server/shine_api/signup_simulation.scala
+++ server/shine_api/signup_simulation.scala
@@ -7,25 +7,47 @@ import com.excilys.ebi.gatling.http.Headers.Names._
 import akka.util.duration._
 import bootstrap._
 
+import Shine.APIKey._
+import Shine.APIURL._
+import Shine.NewUserInfo._
+
 class SignupSimulation extends Simulation {
-	import Shine.APIKey._
-	import Shine.APIURL._
-	import Shine.NewUserInfo._
+	def signUpForADuration(users: Int, duration: Int = 0, rampUp: Int = 10) {
+		val httpConf = httpConfig.baseURL(apiUrl("baseUrl"))
+		val scn = scenario("Signup API")
+			.during(duration) {
+				feed(newUserInfo)
+				.exec(
+					http("Signup request")
+						.post(apiUrl("signup"))
+						.headers(apiKey)
+						.param("email", "${email}")
+						.param("password", "${password}")
+						.param("udid", "${udid}")
+				)
+			}
 
-	val httpConf = httpConfig.baseURL(apiUrl("baseUrl"))
-	val scn = scenario("Signup API")
-		//.during(600) {
-		.repeat(1) {
-			feed(newUserInfo)
-			.exec(
-				http("Signup request")
-					.post(apiUrl("signup"))
-					.headers(apiKey)
-					.param("email", "${email}")
-					.param("password", "${password}")
-					.param("udid", "${udid}")
-			)
-		}
+		setUp(scn.users(users).ramp(rampUp).protocolConfig(httpConf))
+	}
 
-	setUp(scn.users(10).ramp(0).protocolConfig(httpConf))
+	def signUpWithLoopingTimes(users: Int, repeat: Int = 0, rampUp: Int = 10) {
+		val httpConf = httpConfig.baseURL(apiUrl("baseUrl"))
+		val scn = scenario("Signup API")
+			.repeat(repeat) {
+				feed(newUserInfo)
+				.exec(
+					http("Signup request")
+						.post(apiUrl("signup"))
+						.headers(apiKey)
+						.param("email", "${email}")
+						.param("password", "${password}")
+						.param("udid", "${udid}")
+				)
+			}
+
+		setUp(scn.users(users).ramp(rampUp).protocolConfig(httpConf))
+	}
+
+	// signUpForADuration(5, 5, 0)
+	signUpWithLoopingTimes(1, 1, 0)
 }
