4751814e52e59fa48feac66231f6925e8297f685
thy-misfit <thy@misfitwearables.com>
2013-06-20 15:54:36 +0700

    Gatling - new script to push sync logs

--- server/shine_api/api_url.scala
+++ server/shine_api/api_url.scala
@@ -4,5 +4,6 @@ object APIURL {
 	val apiUrl = Map("baseUrl" -> "https://misfit-shine-api-test-1663549078.us-east-1.elb.amazonaws.com",
 		"signup" -> "/shine/v7/signup",
 		"login" -> "/shine/v7/login",
-		"logout" -> "/shine/v7/logout")
+		"logout" -> "/shine/v7/logout",
+		"synclog" -> "/shine/v7/sync_logs")
 }
---
+++ server/shine_api/push_sync_log_simulation.scala
@@ -0,0 +1,36 @@
+package Shine
+
+import com.excilys.ebi.gatling.core.Predef._
+import com.excilys.ebi.gatling.http.Predef._
+import com.excilys.ebi.gatling.jdbc.Predef._
+import com.excilys.ebi.gatling.http.Headers.Names._
+import akka.util.duration._
+import bootstrap._
+
+import Shine.APIKey._
+import Shine.APIURL._
+import Shine.NewSyncLog._
+
+class PushSyncLogSimulation extends Simulation {
+	def pushSyncLogWithLoopingTimes(users: Int, repeat: Int = 0, rampUp: Int = 10) {
+		val httpConf = httpConfig.baseURL(apiUrl("baseUrl"))
+		var header = Map("api_key" -> "76801581",
+						"auth_token" -> "3816438795046890631-fqyBQ8AaybpGzxpygkk5",
+						"Content-Type" -> "application/json")
+		
+		val scn = scenario("Push Sync Log API")
+			.repeat(repeat) {
+				feed(newSyncLog)
+				.exec(
+					http("Push Sync Log Request")
+						.post(apiUrl("synclog"))
+						.headers(header)
+						.body("${log}").asJSON
+						
+				)
+			}
+
+		setUp(scn.users(users).ramp(rampUp).protocolConfig(httpConf))
+	}
+	pushSyncLogWithLoopingTimes(10, 2, 0)
+}
