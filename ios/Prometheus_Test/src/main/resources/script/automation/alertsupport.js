fbc4d492b297d225182fba1eb4575118d37904ca
thy-misfit <thy@misfitwearables.com>
2013-08-19 11:17:21 +0700

    Update alert support

--- ios/Prometheus_Test/src/main/resources/script/automation/alertsupport.js
+++ ios/Prometheus_Test/src/main/resources/script/automation/alertsupport.js
@@ -4,40 +4,35 @@ var AllowLocationMsg = "\"Shine\" Would Like to Use Your Current Location";
 // ======================== handler for Prometheus app ========================
 UIATarget.onAlert = PrometheusAlertHandler;
 
-
-
 // ======================== the alert hander ==================================
 UIALogger.logDebug("Alert Support loaded");
 
-function PrometheusAlertHandler(_alert)
-{
+function PrometheusAlertHandler(_alert) {
 	// get alert title and message
 	var name = _alert.name();
 	var message = _alert.staticTexts()[1].name();
-	
-	if(name === null)
+
+	if (name === null)
 		name = "";
-	
-	if(message === null)
+
+	if (message === null)
 		message = "";
-	
-	if ( name == AllowLocationMsg ||
-		 message == AllowLocationMsg ||
-		 name.indexOf("Current Location") >= 0 || 
-		 message.indexOf("Current Location") >= 0 )
-	{
+
+	if (name == AllowLocationMsg || message == AllowLocationMsg
+			|| name.indexOf("Current Location") >= 0
+			|| message.indexOf("Current Location") >= 0
+			|| name.indexOf("Facebook") || message.indexOf("Facebook")) {
 		UIALogger.logDebug("Alert title: " + name);
 		UIALogger.logDebug("Alert message: " + message);
-	    UIALogger.logDebug("Tap OK");
-	    _alert.buttons()["OK"].tap();
-	    return true;
-    }
-   
-   
-   	UIALogger.logDebug("Do not do anything");
-   	target.delay(5);
+		UIALogger.logDebug("Tap OK");
+		_alert.buttons()["OK"].tap();
+		return true;
+	}
+
+	UIALogger.logDebug("Do not do anything");
+	target.delay(5);
 	return true;
-	
+
 }
 
 target.delay(1000);
