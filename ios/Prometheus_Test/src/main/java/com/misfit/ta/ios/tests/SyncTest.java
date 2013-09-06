31297eb26f861ba57d53540730a51ce84987bbaf
thy-misfit <thy@misfitwearables.com>
2013-09-06 16:02:50 +0700

    Grouping for syncing tests

--- ios/Prometheus_Test/src/main/java/com/misfit/ta/ios/tests/SyncTest.java
+++ ios/Prometheus_Test/src/main/java/com/misfit/ta/ios/tests/SyncTest.java
@@ -23,7 +23,7 @@ import com.misfit.ta.utils.ShortcutsTyper;
 public class SyncTest extends AutomationTest {
 	private static int NUMBER_OF_SYNC = 50;
 
-	@Test(groups = { "iOS", "Prometheus", "iOSAutomation", "Syncing" })
+	@Test(groups = { "iOS", "Prometheus", "iOSAutomation", "Syncing", "Linking" })
 	public void LinkNoShineAvailable() throws InterruptedException, StopConditionException, IOException {
 		ModelHandler model = getModelhandler();
 		model.add("LinkNoShineAvailable", new LinkNoShineAvailableAPI(this, Files.getFile("model/sync/LinkNoShineAvailable.graphml"), false, new NonOptimizedShortestPath(new EdgeCoverage(1.0)), false));
@@ -33,7 +33,7 @@ public class SyncTest extends AutomationTest {
 		System.out.println(actualResult);
 	}
 
-	@Test(groups = { "iOS", "Prometheus", "iOSAutomation", "Syncing" })
+	@Test(groups = { "iOS", "Prometheus", "iOSAutomation", "Syncing", "Linking" })
 	public void LinkOneShineAvailable() throws InterruptedException, StopConditionException, IOException {
 		ModelHandler model = getModelhandler();
 		model.add("LinkOneShineAvailable", new LinkOneShineAvailableAPI(this, Files.getFile("model/sync/LinkOneShineAvaiable.graphml"), false, new NonOptimizedShortestPath(new EdgeCoverage(1.0)), false));
@@ -43,7 +43,7 @@ public class SyncTest extends AutomationTest {
 		System.out.println(actualResult);
 	}
 
-	@Test(groups = { "iOS", "Prometheus", "Syncing" })
+	@Test(groups = { "iOS", "Prometheus", "Syncing", "iOSAutomation", "ContinuousSyncing" })
 	public void SyncContinously() throws InterruptedException, StopConditionException, IOException {
 		
 		// set up: link shine v14 to v14 account
