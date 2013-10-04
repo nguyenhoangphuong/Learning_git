c4e0ecd42077189b4014cb5104feb7ff0571a8a2
Hai Nguyen <hainguyen@misfitwearables.com>
2013-10-04 14:38:06 +0700

    Change upgrade tests so that it can work on jenkins

--- ios/Prometheus_Test/src/main/java/com/misfit/ta/ios/tests/UpgradeAppTest.java
+++ ios/Prometheus_Test/src/main/java/com/misfit/ta/ios/tests/UpgradeAppTest.java
@@ -1,5 +1,6 @@
 package com.misfit.ta.ios.tests;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -20,26 +21,29 @@ public class UpgradeAppTest extends AutomationTest
     @Test(groups = { "iOS", "Prometheus", "HomeScreen", "iOSAutomation", "Upgrade", "ProductionOnly" })
     public void UpgradeFromMVP16_1() throws InterruptedException, StopConditionException, IOException
     {
-        testUpgradeFromApp("apps/mvp16.1/Prometheus.ipa", true);
-        testUpgradeFromApp("apps/mvp16.1/Prometheus.ipa", false);
+    	File mvp = Files.getFile("apps/mvp16.1/Prometheus.ipa");
+        testUpgradeFromApp(mvp, true);
+        testUpgradeFromApp(mvp, false);
     }
     
     @Test(groups = { "iOS", "Prometheus", "HomeScreen", "iOSAutomation", "DayProgress", "ProductionOnly" })
     public void UpgradeFromMVP17_1() throws InterruptedException, StopConditionException, IOException
     {
-    	testUpgradeFromApp("apps/mvp17.1/Prometheus.ipa", true);
-    	testUpgradeFromApp("apps/mvp17.1/Prometheus.ipa", false);
+    	File mvp = Files.getFile("apps/mvp17.1/Prometheus.ipa");
+        testUpgradeFromApp(mvp, true);
+        testUpgradeFromApp(mvp, false);
     }
         
     //@Test(groups = { "iOS", "Prometheus", "HomeScreen", "iOSAutomation", "DayProgress", "ProductionOnly" })
     public void UpgradeFromMVP18_1() throws InterruptedException, StopConditionException, IOException
     {
-    	testUpgradeFromApp("apps/mvp18.1/Prometheus.ipa", true);
-    	testUpgradeFromApp("apps/mvp18.1/Prometheus.ipa", false);
+    	File mvp = Files.getFile("apps/mvp18.1/Prometheus.ipa");
+        testUpgradeFromApp(mvp, true);
+        testUpgradeFromApp(mvp, false);
     }
 
     // test helpers
-    public void testUpgradeFromApp(String pathToApp, boolean willSignOutBeforeUpgrade)
+    public void testUpgradeFromApp(File oldMVPFile, boolean willSignOutBeforeUpgrade)
     	throws FileNotFoundException, StopConditionException, InterruptedException {
     
     	ModelHandler model = getModelhandler();
@@ -47,7 +51,7 @@ public class UpgradeAppTest extends AutomationTest
         		Files.getFile("model/upgrade/UpgradeApp.graphml"),
                 true, new NonOptimizedShortestPath(new EdgeCoverage(1.0)), false);
         
-        api.pathToOldApp = pathToApp;
+        api.pathToOldApp = oldMVPFile.getAbsolutePath();
         api.willSignOutBeforeUpgrade = willSignOutBeforeUpgrade;
         
         model.add("UpgradeAfterSignout", api);
