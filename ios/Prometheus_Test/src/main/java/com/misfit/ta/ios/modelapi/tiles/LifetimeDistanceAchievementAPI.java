8069f092ef43923c4baa2e7d2349ef1d1ed31892
Hai Nguyen <hainguyen@misfitwearables.com>
2013-11-01 11:29:01 +0700

    Fix Lifetime achievement start time.

--- ios/Prometheus_Test/src/main/java/com/misfit/ta/ios/modelapi/tiles/LifetimeDistanceAchievementAPI.java
+++ ios/Prometheus_Test/src/main/java/com/misfit/ta/ios/modelapi/tiles/LifetimeDistanceAchievementAPI.java
@@ -6,7 +6,9 @@ import org.graphwalker.generators.PathGenerator;
 import org.testng.Assert;
 
 import com.misfit.ta.backend.api.MVPApi;
+import com.misfit.ta.backend.aut.BackendHelper;
 import com.misfit.ta.backend.data.goal.Goal;
+import com.misfit.ta.gui.DefaultStrings;
 import com.misfit.ta.gui.Gui;
 import com.misfit.ta.gui.HomeScreen;
 import com.misfit.ta.gui.HomeSettings;
@@ -71,6 +73,13 @@ public class LifetimeDistanceAchievementAPI extends ModelAPI {
 
 		// api: reset current goal
 		MVPApi.updateGoal(token, blankGoal);
+		
+		// api: reset lifetime distance
+		BackendHelper.setLifetimeDistance(token, 0d);
+		
+		// refresh
+		HomeScreen.pullToRefresh();
+		PrometheusHelper.waitForViewToDissappear("UILabel", DefaultStrings.LoadingLabel);
 	}
 
 	
@@ -119,17 +128,27 @@ public class LifetimeDistanceAchievementAPI extends ModelAPI {
 		Gui.swipeUp(1000);
 		Gui.swipeUp(1000);
 		Gui.swipeUp(1000);
-
-		String title = String.format("%d:%02dam", hour, hitAchievementMinute);
 		
-		String[] messages = usUnit ? Timeline.LifetimeDistanceInUSUnitMessages : Timeline.LifetimeDistanceInSIUnitMessages;
-		String message = messages[marathonsNumber == 2 ? 0 : (marathonsNumber == 6 ? 1 : (marathonsNumber == 12? 2 : 0))];
+		int i = -1;
+		for(i = -1; i <= 1; i++) {
 
-		Timeline.openTile(title);
-		Gui.captureScreen("streaktile-" + System.nanoTime());
-		Assert.assertTrue(Timeline.isLifetimeDistanceBadgeTileCorrect(title, marathonsNumber, message));
-		Timeline.closeCurrentTile();
-		Timeline.dragDownTimeline();
+			String title = String.format("%d:%02dam", hour, hitAchievementMinute + i);
+			if(!Timeline.isTileExisted(title))
+				continue;
+
+			String[] messages = usUnit ? Timeline.LifetimeDistanceInUSUnitMessages : Timeline.LifetimeDistanceInSIUnitMessages;
+			String message = messages[marathonsNumber == 2 ? 0 : (marathonsNumber == 6 ? 1 : (marathonsNumber == 12? 2 : 0))];
+
+			Timeline.openTile(title);
+			Gui.captureScreen("streaktile-" + System.nanoTime());
+			Assert.assertTrue(Timeline.isLifetimeDistanceBadgeTileCorrect(title, marathonsNumber, message));
+			Timeline.closeCurrentTile();
+			Timeline.dragDownTimeline();
+			return;
+		}
+		
+		if(i > 1)
+			Assert.fail("No achievement tile");
 	}
 
 	private void changeDistanceSettings(boolean usUnit) {
