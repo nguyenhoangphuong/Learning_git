d0791864ce7bdc9d844f9070cff50d81a45ca78f
Hai Nguyen <hainguyen@misfitwearables.com>
2014-04-03 15:04:26 +0700

    Clean up some resources in shine-backend

--- shine-backend/Prometheus-Backend/src/main/resources/script/old_scripts/automation/smoketest.js
+++
@@ -1,7 +0,0 @@
-var target = UIATarget.localTarget();
-var app = target.frontMostApp();
-
-UIALogger.logDebug("First string");
-UIALogger.logDebug("Second string");
-UIALogger.logDebug("Third string");
-UIALogger.logDebug("Fourth string");
