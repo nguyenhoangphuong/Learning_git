f0fb239a9c7844959aafabea8cb60ec97369c9e6
Giang Nguyen <giang@misfit.com>
2014-07-16 15:20:39 +0700

    Added arguments to model generate script

--- ios/Prometheus_Test/model-tools/generate.sh
+++ ios/Prometheus_Test/model-tools/generate.sh
@@ -2,10 +2,15 @@
 #
 echo "Generating java class for the model"
 
-MYPATH=../src/main/resources/model/homescreen/SleepTileRemoving
-NAME=SleepTileRemoving
+MODEL_HOME=../src/main/resources/model/
+
+graph_path=$2
+MYPATH=$MODEL_HOME$graph_path
+
+NAME=$1
+
 echo Checking size of model
 java -jar graphwalker-standalone.jar offline -f "$MYPATH".graphml -g Shortest_non_optimized -x -s edge_coverage:100 | wc -l
+
 echo "Generating java class"
 java  -jar graphwalker-standalone.jar Source -f "$MYPATH".graphml -t junit.template > "$NAME"API.java
-
