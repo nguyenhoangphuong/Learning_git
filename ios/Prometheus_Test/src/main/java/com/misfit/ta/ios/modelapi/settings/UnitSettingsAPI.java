3c7b982b64772ad896fb8f841e368ce59a6968ad
Minh Trung Hoang Quoc <trunghoang@misfit.com>
2014-08-22 09:54:52 +0700

    Update test case UnitSetting

--- ios/Prometheus_Test/src/main/java/com/misfit/ta/ios/modelapi/settings/UnitSettingsAPI.java
+++ ios/Prometheus_Test/src/main/java/com/misfit/ta/ios/modelapi/settings/UnitSettingsAPI.java
@@ -23,91 +23,95 @@ public class UnitSettingsAPI extends ModelAPI {
 
 	private boolean isDistanceUSUnit = true;
 	private boolean isWeightUSUnit = true;
-	
+
 	private String heightDefaultInUS = "5'4\\\"";
 	private String heightDefaultInSI = "1.63 m";
 	private String weightDefaultInUS = "140.0 lbs";
 	private String weightDefaultInSI = "63.5 kg";
-	
+
 	public void e_Init() {
-		
+
 		// sign up account with default profile
 		PrometheusHelper.signUpDefaultProfile();
-		
+
 		// input a record
 		HomeScreen.tapOpenManualInput();
-		PrometheusHelper.inputManualRecord(new String[] {"1", "00", "am"}, 5, 500);
+		PrometheusHelper.inputManualRecord(new String[] { "1", "00", "am" }, 5,
+				500);
 		HomeScreen.tapSave();
 		ShortcutsTyper.delayOne();
 		HomeScreen.tapProgressCircle();
 	}
-	
+
 	public void e_ChooseSettings() {
-		
+
 		HomeScreen.tapOpenSettingsTray();
 		HomeScreen.tapSettings();
 		ShortcutsTyper.delayOne();
 	}
-	
+
 	public void e_ChangeProfileDistanceUnit() {
-		
+
 		isDistanceUSUnit = !isDistanceUSUnit;
-		if(isDistanceUSUnit) 
-			HomeSettings.tapMile(); 
-		else 
+		if (isDistanceUSUnit)
+			HomeSettings.tapMile();
+		else
 			HomeSettings.tapKm();
-		
+
 		HomeSettings.tapYourProfile();
 	}
 
 	public void e_ChangeProfileWeightUnit() {
-		
+
 		isWeightUSUnit = !isWeightUSUnit;
-		if(isWeightUSUnit)
+		if (isWeightUSUnit)
 			HomeSettings.tapLbs();
 		else
 			HomeSettings.tapKg();
-		
+
 		HomeSettings.tapYourProfile();
 	}
 
 	public void e_ChangeSettingsDistanceUnit() {
-		
+
 		isDistanceUSUnit = !isDistanceUSUnit;
 		HomeSettings.updateHeight(null, null, isDistanceUSUnit);
 
 		HomeSettings.tapBack();
-		PrometheusHelper.waitForViewToDissappear("UILabel", DefaultStrings.EditProfileTitle);
+		PrometheusHelper.waitForViewToDissappear("UILabel",
+				DefaultStrings.EditProfileTitle);
 	}
 
 	public void e_ChangeSettingsWeightUnit() {
-		
+
 		isWeightUSUnit = !isWeightUSUnit;
 		HomeSettings.updateWeight(null, null, isWeightUSUnit);
-		
+
 		HomeSettings.tapBack();
-		PrometheusHelper.waitForViewToDissappear("UILabel", DefaultStrings.EditProfileTitle);
+		PrometheusHelper.waitForViewToDissappear("UILabel",
+				DefaultStrings.EditProfileTitle);
 	}
 
 	public void e_PressBack() {
-		
+
 		HomeSettings.tapBack();
+		HomeScreen.tapOpenSettingsTray();
 	}
 
-
-	
 	public void v_HomeScreen() {
-			
+
 		String distanceSummary = Gui.getProperty("PTRichTextLabel", 2, "text");
 		Assert.assertTrue(HomeScreen.isToday(), "Current screen is Homescreen");
-		Assert.assertTrue(distanceSummary.contains(isDistanceUSUnit ? "miles" : "km"),
+		Assert.assertTrue(
+				distanceSummary.contains(isDistanceUSUnit ? "miles" : "km"),
 				"Distance summary display in correct unit");
 	}
 
 	public void v_ProfileView() {
 		ShortcutsTyper.delayTime(3000);
 		String weight = isWeightUSUnit ? weightDefaultInUS : weightDefaultInSI;
-		String height = isDistanceUSUnit ? heightDefaultInUS : heightDefaultInSI;
+		String height = isDistanceUSUnit ? heightDefaultInUS
+				: heightDefaultInSI;
 
 		Assert.assertTrue(ViewUtils.isExistedView("UILabel", height),
 				"Height should be " + height);
@@ -117,11 +121,13 @@ public class UnitSettingsAPI extends ModelAPI {
 
 	public void v_SettingsView() {
 		ShortcutsTyper.delayTime(3000);
-		boolean currentDistanceUSUnit = Gui.getProperty("UIButton", DefaultStrings.MileLabel,
-				"isSelected").equals("1") ? true : false;
-		boolean currentWeightUSUnit = Gui.getProperty("UIButton", DefaultStrings.LbsLabel,
-				"isSelected").equals("1") ? true : false;
-		
+		boolean currentDistanceUSUnit = Gui.getProperty("UIButton",
+				DefaultStrings.MileLabel, "isSelected").equals("1") ? true
+				: false;
+		boolean currentWeightUSUnit = Gui.getProperty("UIButton",
+				DefaultStrings.LbsLabel, "isSelected").equals("1") ? true
+				: false;
+
 		Assert.assertTrue(isDistanceUSUnit == currentDistanceUSUnit,
 				"Distance Unit is correct");
 		Assert.assertTrue(isWeightUSUnit == currentWeightUSUnit,
