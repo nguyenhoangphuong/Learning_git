d9a41b422b21ba9586689dd3f0f59f69da100543
Minh Trung Hoang Quoc <trunghoang@misfit.com>
2014-09-25 14:15:49 +0700

    Update test case : LeaderboardTest

--- ios/Prometheus_Test/src/main/java/com/misfit/ta/ios/modelapi/social/board/LeaderboardAPI.java
+++ ios/Prometheus_Test/src/main/java/com/misfit/ta/ios/modelapi/social/board/LeaderboardAPI.java
@@ -22,6 +22,7 @@ import com.misfit.ta.gui.PrometheusHelper;
 import com.misfit.ta.gui.social.LeaderboardView;
 import com.misfit.ta.ios.AutomationTest;
 import com.misfit.ta.modelAPI.ModelAPI;
+import com.misfit.ta.utils.ShortcutsTyper;
 
 public class LeaderboardAPI extends ModelAPI {
 	
@@ -178,11 +179,12 @@ public class LeaderboardAPI extends ModelAPI {
 	
 	public void v_LaunchScreen() {
 		
-		Assert.assertTrue(LaunchScreen.isAtLaunchScreen() || LaunchScreen.isAtInitialScreen(), "Current view is start up view");
+		Assert.assertTrue(LaunchScreen.isAtInitialScreen(), "Current view is start up view");
 	}
 	
 	public void v_HomeScreen() {
 		
+		ShortcutsTyper.delayTime(2000);
 		Assert.assertTrue(HomeScreen.isToday(), "Current view is HomeScreen");
 	}
 	
