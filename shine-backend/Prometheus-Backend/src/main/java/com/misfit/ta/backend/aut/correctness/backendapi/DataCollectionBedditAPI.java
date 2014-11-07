59607388fc629cb990737a20843dc291c0074eb9
Minh Trung Hoang Quoc <trunghoang@misfit.com>
2014-11-07 10:01:55 +0700

    update test case for test api: beddit api of data collection

--- shine-backend/Prometheus-Backend/src/main/java/com/misfit/ta/backend/aut/correctness/backendapi/DataCollectionBedditAPI.java
+++ shine-backend/Prometheus-Backend/src/main/java/com/misfit/ta/backend/aut/correctness/backendapi/DataCollectionBedditAPI.java
@@ -1,60 +1,42 @@
 package com.misfit.ta.backend.aut.correctness.backendapi;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.List;
 
 import org.testng.annotations.Test;
 
-import com.misfit.ta.backend.api.internalapi.MVPDCApi;
 import com.misfit.ta.backend.api.internalapi.MultipartUtility;
 import com.misfit.ta.backend.aut.BackendAutomation;
 
 public class DataCollectionBedditAPI extends BackendAutomation{
 	
+	
 	@Test(groups = { "DataCollection", "BedditAPI" })
-	public void demo(){
+	public void pushBedditDataSuccessfully(){
 		String charset = "UTF-8";
-		File uploadFile1 = new File("/Users/trunghoang/Downloads/sleep_1413745423_1413745440");
-		String requestURL = "https://data.int.api.misfitwearables.com/beddit";
+        File uploadFile1 = new File("/Users/trunghoang/Downloads/beddit-example/sleep_1413745423_1413745440");
 
-		try {
-			MultipartUtility multipart = new MultipartUtility(requestURL, charset);
-			
-			multipart.addFormField("user_id", "qwerty@misfit.com");
-			
-			multipart.addFilePart("file", uploadFile1);
-
-			List<String> response = multipart.finish();
-			
-			System.out.println("SERVER REPLIED:");
-			
-			for (String line : response) {
-				System.out.println(line);
-			}
-		} catch (IOException ex) {
-			System.err.println(ex);
-		}
-		
-//		MVPDCApi.generateBedditDataFile();
-//		try{
-//		 BufferedReader br = new BufferedReader(new FileReader("/Users/trunghoang/Downloads/sleep_1413745423_1413745440"));
-//		 StringBuilder sb = new StringBuilder();
-//		 String line = br.readLine();
-//
-//		 while (line != null) {
-//		     sb.append(line).append("\r\n");
-//		     line = br.readLine();
-//		 }
-//		 String everything = sb.toString();
-//		 System.out.println("Content : " + everything);
-//		}catch(Exception e){
-//			System.out.println("Error!");
-//		}
+        String requestURL = "https://data.int.api.misfitwearables.com/beddit";
+        try {
+            MultipartUtility multipart = new MultipartUtility(requestURL, charset);
+             
+            multipart.addFormField("user_id", "trunghoang");
+            multipart.addFormField("device_model", "Android");
+             
+            multipart.addFilePart("thumbnail", uploadFile1);
+            
+ 
+            List<String> response = multipart.finish();
+             
+            System.out.println("SERVER REPLIED:");
+             
+            for (String line : response) {
+                System.out.println(line);
+            }
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
 	}
+	
 }
