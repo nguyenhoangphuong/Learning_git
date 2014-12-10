47e5e1bc8506343ffc442bf434d441919d05dc88
Minh Trung Hoang Quoc <trunghoang@misfit.com>
2014-12-10 15:03:26 +0700

    demo test main flow metawatch

--- shine-backend/Prometheus-Backend/src/main/java/com/misfit/ta/backend/api/metawatch/MVPDCApi.java
+++ shine-backend/Prometheus-Backend/src/main/java/com/misfit/ta/backend/api/metawatch/MVPDCApi.java
@@ -1,7 +1,10 @@
 package com.misfit.ta.backend.api.metawatch;
 
+import java.util.Locale;
+
 import org.apache.log4j.Logger;
 import org.graphwalker.Util;
+import org.testng.Assert;
 
 import com.google.resting.component.impl.ServiceResponse;
 import com.misfit.ta.Settings;
@@ -10,6 +13,7 @@ import com.misfit.ta.backend.api.RequestHelper;
 import com.misfit.ta.backend.data.BaseParams;
 import com.misfit.ta.backend.data.BaseResult;
 import com.misfit.ta.backend.data.MetaWatch.MetaWatchModel;
+import com.misfit.ta.backend.data.MetawatchModel.MetawatchProfileModel;
 import com.misfit.ta.common.MVPCommon;
 import com.misfit.ta.utils.TextTool;
 
@@ -58,4 +62,88 @@ public class MVPDCApi extends RequestHelper {
     	ServiceResponse response = MVPApi.post(url, port, requestInfo);
     	return new BaseResult(response);
     }
+    
+    public static String generateByteDataForMetaWatch() {
+		return bytesToString(new byte[] { 1, 15, 84, 127, -19, -117, 0, 0,
+				1, 26, 10, 20, 100, 15, 10, 20, 100, 15, 10, 20, 100, 15, 10,
+				20, 100, 15, 10, 20, 100, 15, 10, 20, 100, 15, 10, 20, 100, 15,
+				10, 20, 100, 15, 10, 20, 100, 15, 10, 20, 100, 15, 10, 20, 100,
+				15, 10, 20, 100, 15, 10, 20, 100, 15, 10, 20, 100, 15, 10, 20,
+				100, 15, 10, 20, 100, 15, 10, 20, 100, 15, 10, 20, 100, 15, 10,
+				20, 100, 15, 10, 20, 100, 15, 10, 20, 100, 15, 10, 20, 100, 15,
+				10, 20, 100, 15, 10, 20, 100, 15, 10, 20, 100, 15, 10, 20, 100,
+				15, 10, 20, 100, 15, 10, 20, 100, 15, 10, 20, 100, 15, 10, 20,
+				100, 15, 10, 20, 100, 15, 10, 20, 100, 15, 10, 20, 100, 15, 10,
+				20, 100, 15, 10, 20, 100, 15, 10, 20, 100, 15, 10, 20, 100, 15,
+				10, 20, 100, 15 });
+	}
+	
+	private static String bytesToString(byte[] bytes, String seperator) {
+		if (bytes == null || bytes.length == 0)
+			return null;
+
+		String format = "%02X";
+		String formatWithSeperator = format + seperator;
+
+		StringBuilder sb = new StringBuilder();
+
+		int i = 0;
+		while (i < bytes.length - 1) {
+			sb.append(String.format(Locale.US, formatWithSeperator, bytes[i++]));
+		}
+		if (i == bytes.length - 1) {
+			sb.append(String.format(Locale.US, format, bytes[i]));
+		}
+		return sb.toString().toLowerCase(Locale.US);
+	}
+	
+	private static String bytesToString(byte[] bytes) {
+		return bytesToString(bytes, "");
+	}
+	
+	public static void main(String[] args){
+		String accessKeyId = "56915349444458734-yGW6kkeeWJsdlkfi23jifjkdflkfkjf8";
+		String secretKey = "eqFNHUAshCeQIZgSc48Yh80bTo9OVSYXkMfThspX";
+		String userId = MVPApi.generateLocalId();
+		String password = "qwerty";
+		
+//		MetaWatchModel metaWatch = new MetaWatchModel();
+//		metaWatch.setData(generateByteDataForMetaWatch());
+//		metaWatch.setUserId(userId);
+//		metaWatch.setDeviceModel(MVPDCApi.generateDeviceModel());
+//		
+//		metaWatch.setSignature(metaWatch.calSignature(secretKey));
+//		BaseResult result = MVPDCApi.pushMetaWatch(accessKeyId, metaWatch);
+		
+		// Step to get result
+		System.err.println("-----Sign up misfit account");
+		// sign up
+		String email = MVPApi.generateUniqueEmail();
+		MVPApi.signUp(email, password);
+//		System.err.println("-----Register");
+//		// register
+//		MetawatchApi.registerMetawatch("token", userId);
+//		// sign up && create profile
+//		// create profile
+//		System.err.println("-----Profile");
+//		MetawatchProfileModel profile = MetawatchApi.DefaultProfile();
+//		BaseResult result = MetawatchApi.createProfileMetawatch(email, password, profile,
+//				userId);
+//		// call get method
+//		String response = result.response.getResponseString();
+//		String url = response.substring(response.indexOf('h'));
+//
+//		result = MetawatchApi.getExchangeMetawatch(url);
+		System.err.println("-----Sign in meta watch account");
+		MetawatchApi.signInMetawatch(userId, email, password, true);
+		System.err.println("-----Push data");
+		// push data
+		MetaWatchModel metaWatch = new MetaWatchModel();
+		metaWatch.setData(generateByteDataForMetaWatch());
+		metaWatch.setUserId(userId);
+		metaWatch.setDeviceModel(MVPDCApi.generateDeviceModel());
+		
+		metaWatch.setSignature(metaWatch.calSignature(secretKey));
+		MVPDCApi.pushMetaWatch(accessKeyId, metaWatch);
+	}
 }
